07fb3c61fbf588bb47cd3670a297d77083a4fc57
Bryan Stapleton <stapleton_bryan@yahoo.com>
2022-12-18 18:28:50 -0700

    working bg, voice wip

--- davi/src/app/App.tsx
+++ davi/src/app/App.tsx
@@ -1,18 +1,105 @@
+import { useEffect, useRef, useState } from "react";
 import { Button } from "@mantine/core";
-import { useEffect } from "react";
 import useSpeech from "../hooks/useSpeech";
-
+import * as THREE from "three";
 import "./App.css";
 
 function App() {
-  const [speechController] = useSpeech({ lang: "en-US" }),
-    controller = speechController();
+  const [speechController, recognizer] = useSpeech({ lang: "en-US" });
+  var controller = speechController();
+  var renderer = new THREE.WebGLRenderer();
+  var scene = new THREE.Scene();
+  var camera = new THREE.PerspectiveCamera(
+    100,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    2000
+  );
+
   useEffect(() => {
     controller.init();
+    recognizer && recognizer.start();
+  }, [recognizer]);
+  const mountRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    mountRef!.current!.appendChild(renderer.domElement);
+    scene.background = new THREE.Color("rgb(25,35,55)");
+
+    const geometry = new THREE.SphereGeometry(12, 64, 32);
+    const lineMaterial = new THREE.LineBasicMaterial({
+      color: new THREE.Color("rgb(25,25,25)"),
+      fog: true,
+    });
+
+    const vertices = [];
+
+    for (let i = 0; i < 500; i++) {
+      const x = THREE.MathUtils.randFloatSpread(2);
+      const y = THREE.MathUtils.randFloatSpread(2);
+      const z = THREE.MathUtils.randFloatSpread(90);
+
+      vertices.push(x, y, z);
+    }
+
+    const pointSpreadGeometry = new THREE.BufferGeometry();
+    pointSpreadGeometry.setAttribute(
+      "position",
+      new THREE.Float32BufferAttribute(vertices, 3)
+    );
+
+    const material = new THREE.PointsMaterial({
+      size: 0.15,
+    });
+
+    const backgroundPoints = new THREE.Points(pointSpreadGeometry, material);
+    const backgroundRing = new THREE.Points(
+      new THREE.RingGeometry(20, 20, 200),
+      material
+    );
+    scene.add(backgroundPoints, backgroundRing);
+    scene.fog = new THREE.Fog("rgb(10,120,225)", 0, 20);
+
+    camera.position.z = 50;
+    let ringScale = 1.0;
+    let toggle = 0;
+    var animate = function () {
+      requestAnimationFrame(animate);
+
+      backgroundPoints.rotation.z += 0.002;
+      backgroundRing.rotation.z -= 0.001;
+      backgroundRing.scale.set(ringScale, ringScale, ringScale);
+      toggle === 0 ? (ringScale += 0.001) : (ringScale -= 0.001);
+
+      ringScale < 0.5 ? (toggle = 0) : ringScale > 0.59 ? (toggle = 1) : null;
+
+      renderer.render(scene, camera);
+    };
+
+    let onWindowResize = function () {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+    };
+
+    window.addEventListener("resize", onWindowResize, false);
+
+    animate();
+    return () => {
+      mountRef!.current!.removeChild(renderer.domElement);
+    };
   }, []);
   return (
     <div className="App">
-      <Button />
+      <Button
+        sx={{ position: "absolute", left: "25%", top: "25%" }}
+        onClick={() => {
+          recognizer!.start();
+        }}
+      />
+      <div ref={mountRef} />
+      <span>test test</span>
     </div>
   );
 }
--- davi/src/hooks/useSpeech.ts
+++ davi/src/hooks/useSpeech.ts
@@ -5,7 +5,7 @@ export interface SpeechProps {
   auto: boolean;
 }
 
-const useSpeech = ({ lang, auto }: Partial<SpeechProps>) => {
+const useSpeech = ({ lang, auto }: Partial<SpeechProps>): [any, any] => {
   const [initialized, toggleInit] = useState<boolean>(false);
   const [recognizer, setRecognizer] = useState(null);
 
@@ -13,13 +13,31 @@ const useSpeech = ({ lang, auto }: Partial<SpeechProps>) => {
     const init = () => {
       if (!initialized) {
         toggleInit(true);
+        const grammar =
+          "#JSGF V1.0; grammar colors; public <color> = aqua | azure | beige | bisque | black | blue | brown | chocolate | coral | crimson | cyan | fuchsia | ghostwhite | gold | goldenrod | gray | green | indigo | ivory | khaki | lavender | lime | linen | magenta | maroon | moccasin | navy | olive | orange | orchid | peru | pink | plum | purple | red | salmon | sienna | silver | snow | tan | teal | thistle | tomato | turquoise | violet | white | yellow ;";
         let speechRecognizer = new ((window as any).webkitSpeechRecognition ||
           (window as any).mozSpeechRecognition ||
           (window as any).msSpeechRecognition ||
           (window as any).oSpeechRecognition ||
           (window as any).SpeechRecognition)();
-        speechRecognizer.language = lang || "en-US";
-        speechRecognizer.autoplay = auto || false;
+        const grammarList = new (window as any).webkitSpeechGrammarList();
+        grammarList.addFromString(grammar, 1);
+        speechRecognizer.grammars = grammarList;
+        speechRecognizer.continuous = true;
+        speechRecognizer.lang = lang || "en-US";
+        speechRecognizer.autoplay = auto || true;
+        speechRecognizer.onstart = () => {
+          console.log("started listening");
+        };
+        speechRecognizer.onsoundstart = () => {
+          console.log("sound detected");
+        };
+        speechRecognizer.onresult = (e: Event) => {
+          console.log(e.target);
+        };
+        speechRecognizer.onend = () => {
+          console.log("ended");
+        };
         setRecognizer(speechRecognizer);
       } else {
         return console.error("already initialized");
@@ -27,11 +45,10 @@ const useSpeech = ({ lang, auto }: Partial<SpeechProps>) => {
     };
 
     return {
-      recognizer,
       init,
     };
   };
-  return [SpeechController];
+  return [SpeechController, recognizer];
 };
 
 export default useSpeech;
